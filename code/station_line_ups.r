0dbc89ad5eebdee13ed14eec94d011acdffa7c0c
pdowty <peter.dowty@dnr.wa.gov>
2025-07-26 21:53:20 -0700

    Started new script for station floating bar line ups.

---
+++ code/station_line_ups.r
@@ -0,0 +1,25 @@
+###############################################################################
+#  
+#  Make station "line up" graphs.
+#  These are floating bar graphs where bars indicate range of data for each
+#  station for a given month range/season.
+#
+#  Input data must be already loaded in the workspace.
+#
+#  July 2025
+#
+###############################################################################
+
+library(tidyverse)
+
+
+
+# specify variable
+
+
+# add season variable - for use in facetting
+
+
+# group by station and season
+
+# for each group, get min, max, median, mean
