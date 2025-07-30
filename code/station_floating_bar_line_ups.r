25c266b6d5102c9ba042307e8b0c34b47105b849
Pete Dowty <peter.dowty@dnr.wa.gov>
2025-07-29 17:23:09 -0700

    Updated floating bar line ups.

--- code/station_floating_bar_line_ups.r
+++ code/station_floating_bar_line_ups.r
@@ -14,7 +14,6 @@
 library(tidyverse)
 
 
-
 # specify variable by index:
 # 1-5:   "PO4","SiOH4","NH4","NO2","NO3",
 # 6-10:  "Xmiss_25cm","BatC","FluorAdjusted","Turb","DOAdjusted",
@@ -22,34 +21,60 @@ library(tidyverse)
 var_index <- 14
 
 
+# Open table with distance-from-ocean and HSIL flag, join and filter for HSIL
+filepath2 <- str_c("output_tables","ecy_stations_distance_export.csv", sep="/")
+ecy_stn_dist <- read.csv(filepath2, stringsAsFactors=FALSE)
+ecy_stn_dist_sel <- ecy_stn_dist %>% select(Station, subgroup, ocean_dist_km)
+ecy_meas_qa_jn <- ecy_meas_qa %>% 
+  left_join(ecy_stn_dist_sel, by="Station") %>%
+  filter(subgroup=="HSIL_study_area") %>%
+  select(all_of(varList[var_index]), Depth, obs_index, Station, date, subgroup)
+
 # add season variable - for use in facetting
-ecy_meas_qa_season <- ecy_meas_qa %>%
+ecy_meas_qa_season <- ecy_meas_qa_jn %>%
   mutate(season = case_when(
     month(date) < 4 ~ "Jan-Feb-Mar",
     month(date) < 7 ~ "Apr-May-Jun",
     month(date) < 10 ~ "Jul-Aug-Sep",
     month(date) < 13 ~ "Oct-Nov-Dec" 
   ))
+seas_levels <- c("Jan-Feb-Mar", "Apr-May-Jun", "Jul-Aug-Sep", "Oct-Nov-Dec")
+ecy_meas_qa_season <- ecy_meas_qa_season %>%
+  mutate(season_fct = factor(season, levels=seas_levels)) %>%
+  select(-season)
+
+# Select 0-10 m depth
+ecy_meas_qa_season_10m <- ecy_meas_qa_season %>%
+   filter(Depth <= 10.0)
+
+# get station order by distance from ocean and use as factor levels
+ecy_stn_dist_sel_sort <- ecy_stn_dist_sel %>%
+  arrange(ocean_dist_km)
+stn_levels <- ecy_stn_dist_sel_sort$Station
 
 # group by station and season and summarize stats
-ecy_season_stats <- ecy_meas_qa_season %>%
-  group_by(Station, season) %>%
+ecy_season_stats <- ecy_meas_qa_season_10m %>%
+  group_by(Station, season_fct) %>%
   summarize(mean = mean(Temp, na.rm=TRUE),
             min = min(Temp, na.rm=TRUE),
             max = max(Temp, na.rm=TRUE),
             median = median(Temp, na.rm=TRUE)
   )
+ecy_season_stats_fct <- ecy_season_stats %>%
+  mutate(Station_fct = factor(Station, levels=stn_levels))
 
 
 # make floating bar graph with mean or median facetted by season
-p <- ggplot(data=ecy_season_stats,
-            mapping=aes(x=Station, ymin=min, ymax=max, y=median)) +
+p <- ggplot(data=ecy_season_stats_fct,
+            mapping=aes(x=Station_fct, ymin=min, ymax=max, y=median,
+                        fill=median)) +
      geom_crossbar() +
-     facet_wrap(vars(season), nrow=4) +
+     facet_wrap(vars(season_fct), nrow=4) +
      theme_bw() +
      theme(
        axis.text.x = element_text(angle=90)
-     )
+     ) +
+     scale_fill_viridis_d(option="H")
 
 
 
