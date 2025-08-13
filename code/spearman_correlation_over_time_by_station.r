649f98847161cbb85717b320c8af4c269048cd60
Pete Dowty <peter.dowty@dnr.wa.gov>
2025-08-12 17:17:44 -0700

    Start to Spearman code.

---
+++ code/spearman_correlation_over_time_by_station.r
@@ -0,0 +1,13 @@
+###############################################################################
+#  Calculate Spearman correlation and significance for each variable in the
+#  Ecology dataset against time.
+#
+#  Required input:  the data frame ecy_meas_qa must be present in the workspace.
+#  This data frame is created in b_compile_annual_netCDF.r
+#  
+#  August 2025
+#
+###############################################################################
+
+library(tidyverse)
+
