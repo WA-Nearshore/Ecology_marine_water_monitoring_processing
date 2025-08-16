96d6770066409d1cbd0c77dc9a363cd9b4057d90
Pete Dowty <peter.dowty@dnr.wa.gov>
2025-08-15 18:04:46 -0700

    Small step on spearman code.

--- code/spearman_correlation_over_time_by_station.r
+++ code/spearman_correlation_over_time_by_station.r
@@ -11,3 +11,18 @@
 
 library(tidyverse)
 
+
+
+# get enhanced station table exported from GIS with subgroup attribute that can 
+# be used to filter stations in the HSIL study area 
+stn_tbl_path <- str_c("output_tables", "ecy_stations_subgroups_tbl.csv", sep=sepsym)
+stations <- read.csv(stn_tbl_path, stringsAsFactors=FALSE)
+
+
+
+
+# pivot longer so variables are no longer in separate columns
+long_good_data_recs <- ecy_meas_qa %>%
+    pivot_longer(PO4:Temp, names_to = "parameter", values_to = "value") %>%
+    select(Depth,obs_index,station_index,Station,date,parameter,value) %>%
+    drop_na(value)
