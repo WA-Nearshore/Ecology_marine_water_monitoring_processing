895d56cb2cee435803ccb8a7d37ad9cb56876667
Pete Dowty <peter.dowty@dnr.wa.gov>
2025-11-19 17:18:02 -0800

    Working on Feb 2024 departure graph in deg C.

--- code/month_departure_graph.r
+++ code/month_departure_graph.r
@@ -4,11 +4,16 @@
 #  Departures from long-term means are shown for all "Spearman" stations for
 #  one month-of-year.  Each site is depicted as a point in temp-zscore space.
 #
-#  The data frame cy_long_mean_jn must b4e available in the workspace. It is
+#  The data frame ecy_long_mean_jn must be available in the workspace. It is
 #  created by code in spearman_correlation_over_time_by_station.r
-#  November 2025
+#  November 2025.
 #
 ###############################################################################
+
+# if prerequisite 'ecy_long_mean_jn' must be created, run these two lines once
+# i.e. comment lines out if this script is run repeatedly
+# source("code/assemble_netCDF_data.r")
+# source("code/spearman_correlation_over_time_by_station.r")
 
 # set parameters for the graphs
 sel_month <- 2
@@ -16,12 +21,15 @@ sel_year <- 2024
 month_labels <- "February"
 sel_prm <- "Temp"
 
-# Requirements for Spearman
-# Set min number of monthly records for Spearman to be run.
-# Also set min number of years spanned by these monthly records. 
+# Our requirements for Spearman to be run on a station-month.
+# Set min number of monthly records & min number of years spanned 
 min_n_Spearman <- 8
 min_yr_span <- 8
 
+
+######################################################################
+# Filters for month, parameter and Spearman requirements 
+######################################################################
 
 # filter for months and parameter
 ecy_long_mean_jn_filt <- ecy_long_mean_jn %>%
@@ -30,6 +38,11 @@ ecy_long_mean_jn_filt <- ecy_long_mean_jn %>%
 # filter to just stations used for Spearman correlations
 ecy_long_mean_jn_filt2 <- ecy_long_mean_jn_filt %>%
   filter(n_months >= min_n_Spearman, yr_span >= min_yr_span)
+
+
+######################################################################
+# get station-month stats
+######################################################################
 
 # get station-month mean and sd
 ecy_stn_stats <- ecy_long_mean_jn_filt2 %>%
@@ -41,30 +54,41 @@ ecy_long_z <- ecy_long_mean_jn_filt2 %>%
   mutate(zscore = (prm_mean_val - lt_mean)/lt_sd,
          departure = prm_mean_val - lt_mean)
 
-ecy_long_z_fct <- ecy_long_z %>%
-  mutate(month = factor(month))
-levels(ecy_long_z_fct$month) <- month_labels
+
+######################################################################
+# convert stations to factor in order of dist to ocean 
+######################################################################
 
 # Open table with distance-from-ocean and HSIL flag
 filepath2 <- str_c("output_tables","ecy_stations_distance_export.csv", sep="/")
 ecy_stn_dist <- read.csv(filepath2, stringsAsFactors=FALSE)
 ecy_stn_dist_sel <- ecy_stn_dist %>% select(Station, subgroup, ocean_dist_km)
-ecy_meas_qa_jn <- ecy_meas_qa %>% 
-  left_join(ecy_stn_dist_sel, by="Station") %>%
-  filter(subgroup=="HSIL_study_area") %>%
-  select(all_of(varList[var_index]), Depth, obs_index, Station, date, subgroup)
 
+# get station order by distance from ocean to use as factor levels
+ecy_stn_dist_sel_sort <- ecy_stn_dist_sel %>% arrange(ocean_dist_km)
+stn_levels <- ecy_stn_dist_sel_sort$Station
+
+ecy_long_z_fct <- ecy_long_z %>%
+  mutate(Station = factor(Station, levels=stn_levels))
 
 
 # filter for 2024 data
 ecy_long_z_fct_Y <- ecy_long_z_fct %>% filter(year == sel_year)
 
+# make departure graph
 p4 <- ggplot(data = ecy_long_z_fct_Y,
-             mapping = aes(x=zscore, y=departure)) +
-      geom_point(color="gray60", size=2.0) +
+             mapping = aes(x=Station, y=departure)) +
+      geom_segment(aes(x=Station, xend=Station, y=0, yend=departure),
+                   color="gray20", linewidth=0.8) +
+      geom_point(color="sienna", size=2.5) +
+      geom_hline(yintercept=0, color="gray60", linewidth=0.8) +
       theme_bw() +
       theme(
-       panel.grid.major.x = element_line(color="gray"),
        axis.title.y = element_text(margin = margin(r=10)),
-      )
+       axis.title.x = element_blank(),
+       axis.text.x = element_text(angle=90)
+      ) +
+      scale_y_continuous(name = "Departure (deg C)", limits=c(-1.2, 1.2))
+      
+
 
